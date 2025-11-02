d5fd5b6782a662f770a32eb185d7371cb985024c
yhernandeze <yolo.1518h@gmail.com>
2025-11-01 20:27:22 -0500

    update

--- airflow/dags/diabetes_training_pipeline.py
+++ airflow/dags/diabetes_training_pipeline.py
@@ -35,7 +35,8 @@ default_args = {
     'start_date': datetime(2025, 10, 1),
     'email_on_failure': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=2),
+    'max_active_runs': 1,
+    'retry_delay': timedelta(minutes=30),
 }
 
 dag = DAG(
